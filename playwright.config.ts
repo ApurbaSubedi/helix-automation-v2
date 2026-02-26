6194577f575d24f8ab0dbad19196471b4e0b6c77
Apurba <appu.subedi@gmail.com>
2026-02-26 13:45:57 +0545

    staging link added in base url

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: 'https://helix-1.idmc.local.togglecorp.com/sign-in/',
+    baseURL: 'https://helix.staging.tools.idmcdb.org/sign-in/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
